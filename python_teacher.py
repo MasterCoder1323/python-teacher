3129f6ac03fb1692a025c500ab47e7061127ef87
MasterCoder <sparlevi18@gmail.com>
2024-02-28 17:45:02 -0800

    Version 1.1.2

--- python_teacher.py
+++ python_teacher.py
@@ -1,9 +1,13 @@
 # import sys
 def start():
-    version = '1.1.1'
+    version = '1.1.2'
     print('Starting Python Teacher v'+version)
     # Main Loop
-    
+    name = input('Enter your name: ')
+    q = input('Are you ready to begin learning '+name+'? [yes]: ')
+    if 'y' in q.lower():
+        print('Time to Learn Python!')
+        #while True
     
     # Closing
     print('Closing Python Teacher')
