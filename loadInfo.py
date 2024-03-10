2a028af3cdfefee92f65a411694e440ca2adbd9a
MasterCoder <sparlevi18@gmail.com>
2024-03-10 13:28:45 -0700

    Save point

--- loadInfo.py
+++ loadInfo.py
@@ -1,7 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar  4 15:31:45 2024
+import json
 
-@author: mcode
-"""
+def getUserInfo():
+    try:
+        with open("user_info.json", "r") as file:
+            user_info = json.load(file)
+            return user_info
+    except FileNotFoundError:
+        # File not found, prompt user for info and create a new file
+        name = input("Enter your name: ")
+        age = input("Enter your age: ")
+        password = input("Password ")
+        user_info = {f'{name}':{'age':age, 'password': password}}
 
+        with open("user_info.json", "w") as file:
+            json.dump(user_info, file)
+
+        return user_info
+    
+if __name__ == '__main__':
+    userInfo = getUserInfo()
+    print(userInfo)
--- python_teacher.py
+++ python_teacher.py
@@ -1,10 +1,14 @@
 import lessons.no1 as no1
 from rich.prompt import Confirm
+from loadInfo import getUserInfo
 def start():
     version = '1.1.5'
     print('Starting Python Teacher v'+version)
     # Main Loop
-    name = input('Enter your name: ')
+    userInfo = getUserInfo()
+    print('Hi')
+    print('Users: \n'+str(userInfo.keys()))
+    alreadyHere = Confirm.ask('Is your name displayed above?')
     q = Confirm.ask('Are you ready to begin learning '+name+'?')
     if q:
         print('Time to Learn Python!')
